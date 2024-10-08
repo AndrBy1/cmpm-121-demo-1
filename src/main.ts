6ea2303975b25218b97a0983d4ca37ad9e6df10c
Andrew Byi <abyi@ucsc.edu>
2024-10-07 21:13:13 -0700

    added AddToCount function

--- src/main.ts
+++ src/main.ts
@@ -15,14 +15,14 @@ app.append(button);
 
 let count: number = 0;
 button.addEventListener("click", () => {
-    count = AddToCount(count)
-    counter.textContent = `${count} slaps`;
+  count = AddToCount(count);
+  counter.textContent = `${count} slaps`;
 });
 
 const counter = document.createElement("div");
 counter.textContent = `${count} slaps`;
 app.append(counter);
 
-function AddToCount(num: number){
-    return num += 1
+function AddToCount(num: number) {
+  return (num += 1);
 }

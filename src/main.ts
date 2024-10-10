e4b9b9894a80e51cbdfb7e6ec885f402c4e17469
Andrew Byi <abyi@ucsc.edu>
2024-10-09 19:35:39 -0700

    fixed up some code

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ document.title = gameName;
 
 let count: number = 0;
 let upgradeLvl: number;
-let upgradenum: number[] = [0, 0, 0];
+let ratenum = 0;
+const upgradenum: number[] = [0, 0, 0];
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
@@ -18,15 +19,15 @@ button.textContent = "🫨"; //shake emoji
 app.append(button);
 
 const upgradeAButton = document.createElement("button");
-upgradeAButton.textContent = "upgrade";
+upgradeAButton.textContent = "upgrade A";
 app.append(upgradeAButton);
 
 const upgradeBButton = document.createElement("button");
-upgradeBButton.textContent = "upgrade";
+upgradeBButton.textContent = "upgrade B";
 app.append(upgradeBButton);
 
 const upgradeCButton = document.createElement("button");
-upgradeCButton.textContent = "upgrade";
+upgradeCButton.textContent = "upgrade C";
 app.append(upgradeCButton);
 
 const counter = document.createElement("div");
@@ -34,7 +35,7 @@ counter.textContent = `${count} shakes`;
 app.append(counter);
 
 const rateCounter = document.createElement("div");
-//rateCounter.textContent = `${ratenum} shakes/sec`;
+rateCounter.textContent = `${ratenum} shakes/sec`;
 app.append(rateCounter);
 
 button.addEventListener("click", () => {
@@ -54,7 +55,7 @@ upgradeBButton.addEventListener("click", () => {
   upgradeLvl = 2;
   upgradenum[1]++;
   if (count >= 10) {
-    AddToCount(-10);
+    AddToCount(-100);
     upgrade();
   }
 });
@@ -63,36 +64,32 @@ upgradeCButton.addEventListener("click", () => {
   upgradeLvl = 3;
   upgradenum[2]++;
   if (count >= 10) {
-    AddToCount(-10);
+    AddToCount(-1000);
     upgrade();
   }
 });
 
-/*
-const autoClick = setInterval(() => {
-  AddToCount();
-}, 600);*/
-
 function upgrade() {
+  rateCounter.textContent = `${ratenum} shakes/sec`;
   requestAnimationFrame(animate);
 }
 
 function animate() {
-  let perSec;
+  console.log("ratenum" + ratenum);
   if (upgradeLvl == 1) {
-    perSec = 0.1;
+    ratenum += 0.1;
   } else if (upgradeLvl == 2) {
-    perSec = 2;
-  } else {
-    perSec = 50;
+    ratenum += 2;
+  } else if (upgradeLvl == 3){
+    ratenum += 50;
   }
   const frame = performance.now();
   console.log(frame);
   requestAnimationFrame(animate);
   if (Math.floor(frame) % 1000 <= 6) {
-    AddToCount(perSec);
+    AddToCount(ratenum);
   }
-  perSec = 0;
+  upgradeLvl = 0;
 }
 
 function AddToCount(inc: number) {

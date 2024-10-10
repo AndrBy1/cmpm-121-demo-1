d7f1e3455d251f644076140b74c68429a2f16501
Andrew Byi <abyi@ucsc.edu>
2024-10-09 21:18:20 -0700

    Step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -47,17 +47,18 @@ button.addEventListener("click", () => {
 
 upgradeAButton.addEventListener("click", () => {
   upgradeLvl = 1;
-  upgradenum[0]++;
-  if (count >= 10) {
+  if (count >= 10 * (1.15 * upgradenum[0])) {
+    upgradenum[0]++;
     AddToCount(-10);
     upgrade();
   }
+  
 });
 
 upgradeBButton.addEventListener("click", () => {
   upgradeLvl = 2;
-  upgradenum[1]++;
-  if (count >= 100) {
+  if (count >= 100 * (1.15 * upgradenum[1])) {
+    upgradenum[1]++;
     AddToCount(-100);
     upgrade();
   }
@@ -65,8 +66,8 @@ upgradeBButton.addEventListener("click", () => {
 
 upgradeCButton.addEventListener("click", () => {
   upgradeLvl = 3;
-  upgradenum[2]++;
-  if (count >= 1000) {
+  if (count >= 1000 * (1.15 * upgradenum[2])) {
+    upgradenum[2]++;
     AddToCount(-1000);
     upgrade();
   }
@@ -79,16 +80,16 @@ function upgrade() {
 }
 
 function animate() {
-  console.log("ratenum" + ratenum);
+  //console.log("ratenum" + ratenum);
   if (upgradeLvl == 1) {
-    ratenum += 0.1;
+    ratenum += 0.1
   } else if (upgradeLvl == 2) {
-    ratenum += 2;
+    ratenum += 2
   } else if (upgradeLvl == 3) {
-    ratenum += 50;
+    ratenum += 50
   }
   const frame = performance.now();
-  console.log(frame);
+  //console.log(frame);
   requestAnimationFrame(animate);
   if (Math.floor(frame) % 1000 <= 6) {
     AddToCount(ratenum);

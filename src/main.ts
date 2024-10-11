53a107a8e47742ea1b2fbaad665cf8b8dd4b725d
Andrew Byi <abyi@ucsc.edu>
2024-10-10 17:24:44 -0700

    fixed a lot of code, added negotiator button

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ let count: number = 0;
 let ratenum = 0;
 let perClick = 1;
 //let frameNum = 0;
-const upgradenum: number[] = [0, 0, 0, 0];
+const upgradenum: number[] = [0, 0, 0, 0, 0];
 
 interface Item {
   name: string;
@@ -48,6 +48,10 @@ const upgradeDButton = document.createElement("button");
 upgradeDButton.textContent = `Purchase driller (50 rocks)`;
 app.append(upgradeDButton);
 
+const upgradeEButton = document.createElement("button");
+upgradeEButton.textContent = `Purchase negotiator (150 rocks)`;
+app.append(upgradeEButton);
+
 const counter = document.createElement("div");
 counter.textContent = `${count} Moon Rocks`;
 app.append(counter);
@@ -59,6 +63,9 @@ app.append(rateCounter);
 const drillDesc = document.createElement("div");
 drillDesc.textContent = `drills increases rocks per click by 1`;
 
+const negotiDesc = document.createElement("div");
+negotiDesc.textContent = `negotiator decreases price of everything by 10%, max negotiator is 6`;
+
 const upgradeCounter = document.createElement("div");
 app.append(upgradeCounter);
 
@@ -68,41 +75,55 @@ button.addEventListener("click", () => {
 
 upgradeDButton.addEventListener("click", () => {
   app.append(drillDesc);
-  if (count >= 50){
-    count -= 50;
+  if (count >= 50  * Math.pow(1.15, upgradenum[3])) {
+    AddToCount(-50 * Math.pow(1.15, upgradenum[3]));
     upgradenum[3]++;
     perClick++;
-    upgradeCounter.textContent = `${availableItems[0].name}: ${upgradenum[0]}, ${availableItems[1].name}: ${upgradenum[1]}, ${availableItems[2].name}: ${upgradenum[2]}, driller: ${upgradenum[3]}`;
+    upgradeCounter.textContent = `${availableItems[0].name}: ${upgradenum[0]}, ${availableItems[1].name}: ${upgradenum[1]}, ${availableItems[2].name}: ${upgradenum[2]}, driller: ${upgradenum[3]}, negotiator: ${upgradenum[4]}`;
+  }
+});
+
+upgradeEButton.addEventListener("click", () => {
+  app.append(negotiDesc);
+  if (count >= 150 * Math.pow(1.15, upgradenum[4]) && upgradenum[4] <= 6) {
+    AddToCount(-150 * Math.pow(1.15, upgradenum[4]));
+    upgradenum[4]++;
+    upgradeCounter.textContent = `${availableItems[0].name}: ${upgradenum[0]}, ${availableItems[1].name}: ${upgradenum[1]}, ${availableItems[2].name}: ${upgradenum[2]}, driller: ${upgradenum[3]}, negotiator: ${upgradenum[4]}`;
   }
 });
 
 upgradeAButton.addEventListener("click", () => {
   //upgradeLvl = 1;
-  console.log(availableItems[0].cost * Math.pow(1.15, upgradenum[0]))
-  if (count >= availableItems[0].cost * Math.pow(1.15, upgradenum[0])) {
+  const calculate = availableItems[0].cost * Math.pow(1.15, upgradenum[0]);
+  console.log(calculate - calculate * (0.1 * upgradenum[4]));
+  if (count >= (calculate - calculate * (0.1 * upgradenum[4]))) {
     ratenum += availableItems[0].rate;
     upgradenum[0]++;
-    AddToCount(-availableItems[0].cost);
+    AddToCount(-(calculate - calculate * (0.1 * upgradenum[4])));
     upgrade();
   }
 });
 
 upgradeBButton.addEventListener("click", () => {
   //upgradeLvl = 2;
-  if (count >= availableItems[1].cost * Math.pow(1.15, upgradenum[1])) {
+  const calculate = availableItems[1].cost * Math.pow(1.15, upgradenum[1]);
+  console.log(calculate - calculate * (0.1 * upgradenum[4]));
+  if (count >= (calculate - calculate * (0.1 * upgradenum[4]))) {
     ratenum += availableItems[1].rate;
     upgradenum[1]++;
-    AddToCount(-availableItems[1].cost);
+    AddToCount(-(calculate - calculate * (0.1 * upgradenum[4])));
     upgrade();
   }
 });
 
 upgradeCButton.addEventListener("click", () => {
   //upgradeLvl = 3;
-  if (count >= availableItems[2].cost * Math.pow(1.15, upgradenum[2])) {
+  const calculate = availableItems[2].cost * Math.pow(1.15, upgradenum[2]);
+  console.log(calculate - calculate * (0.1 * upgradenum[4]));
+  if (count >= (calculate - calculate * (0.1 * upgradenum[4]))) {
     ratenum += availableItems[2].rate;
     upgradenum[2]++;
-    AddToCount(-availableItems[2].cost);
+    AddToCount(-(calculate - calculate * (0.1 * upgradenum[4])));
     upgrade();
   }
 });
@@ -110,13 +131,12 @@ upgradeCButton.addEventListener("click", () => {
 function upgrade() {
   requestAnimationFrame(animate);
   rateCounter.textContent = `${ratenum} Moon Rocks/sec`;
-  upgradeCounter.textContent = `${availableItems[0].name}: ${upgradenum[0]}, ${availableItems[1].name}: ${upgradenum[1]}, ${availableItems[2].name}: ${upgradenum[2]}, driller: ${upgradenum[3]}`;
+  upgradeCounter.textContent = `${availableItems[0].name}: ${upgradenum[0]}, ${availableItems[1].name}: ${upgradenum[1]}, ${availableItems[2].name}: ${upgradenum[2]}, driller: ${upgradenum[3]}, negotiator: ${upgradenum[4]}`;
 }
 
 function animate() {
-  
   //console.log("ratenum" + ratenum);
-  
+
   const frame = performance.now();
   //console.log(frame);
   requestAnimationFrame(animate);
